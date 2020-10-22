db91286062d16a5b5d385f30983a3de8d7b780c0
AndreyShabalin <Andreyshabalin34@gmail.com>
2020-10-22 17:34:30 +0600

    Убрал упоминание метода

--- DatabaseConnect/Form1.Designer.cs
+++ DatabaseConnect/Form1.Designer.cs
@@ -194,7 +194,6 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(125, 20);
             this.textBox2.TabIndex = 3;
-            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // label1
             // 
